f4fb1f1f622f4b0bb39663523143e007b336f2b7
arifbabu <55562558+arifbabu@users.noreply.github.com>
2024-06-01 02:32:48 +0600

    all file upload

---
+++ js/custom.js
@@ -0,0 +1,151 @@
+
+// // Banner Slide-2 using slick
+// $('.banner_slide').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//       arrows: false,
+//       autoplay: false,
+//       speed: 1200,
+//       dots: false,
+//       fade: true,
+//   });
+  
+  
+
+  
+  
+  
+//   // Navigation Top Fixed
+//   $navOffset=$('#navigation').offset().top;
+//   $(window).scroll(function(){
+//       $scrolling=$(this).scrollTop();
+//       if($scrolling > $navOffset){
+//          $('#navigation').addClass('navFixed');
+//       }else{
+//           $('#navigation').removeClass('navFixed');
+//       }
+//   });
+  
+//   // Venobox for pop-up
+//   $('.venobox').venobox()
+  
+  
+//   // Slick Slider For Team Member
+//   $('.team_slide').slick({
+//       dots: false,
+//         arrows: false,
+//         autoplay: true,
+//       speed: 900,
+//       slidesToShow: 4,
+//       slidesToScroll: 1,
+//       responsive: [
+//         {
+//           breakpoint: 1024,
+//           settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 3,
+//             infinite: true,
+//             dots: true
+//           }
+//         },
+//         {
+//           breakpoint: 600,
+//           settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 2
+//           }
+//         },
+//         {
+//           breakpoint: 480,
+//           settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 1
+//           }
+//         }
+//       ]
+//     });
+  
+  
+  // Slick Slider For Testimonial Slide
+  $('.testimonial_slide').slick({
+      dots: true,
+        arrows: false,
+        autoplay: true,
+      speed: 900,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  
+//     // Counter Activation Code
+//   $('.counter').counterUp({
+//     delay: 10,
+//     time: 1000
+//   });
+  
+  
+//   // Partner Logo part slide
+//   $('.partner_slide').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//       arrows: false,
+//       autoplay: true,
+//       speed: 1200,
+//       dots: false,
+//   });
+  
+//   // Scroll to Top Button function
+//   $('.top_to').click(function(){
+//     $('html').animate({
+//         scrollTop: 0
+//     }, 1000);
+//   });
+//   $(window).scroll(function(){
+//     $scrolling=$(this).scrollTop();
+//     if($scrolling > 300){
+//        $('.top_to').fadeIn();
+//     }
+//     else{
+//         $('.top_to').fadeOut();
+//     }
+//   });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
---
+++ js/script.js
@@ -0,0 +1,148 @@
+$(window).scroll(function(){
+    $scroll = $(this).scrollTop();
+    if($scroll >= 200){
+        $('#navigation').addClass('navFixed');
+    }else{
+        $('#navigation').removeClass('navFixed');
+    }
+});
+
+
+  // Navigation Bar Active Class
+  $('body').scrollspy({ target: '#navbarNav' });
+
+
+// Open a URL in a lightbox
+// var lightbox = lity('//www.youtube.com/watch?v=XSGBVzeBUbk');
+
+// Bind as an event handler
+$(document).on('click', '[data-lightbox]', lity);
+
+// Slick Slider For Testimonial Slide
+$('.testimonial_slide').slick({
+    dots: true,
+      arrows: false,
+      autoplay: true,
+    speed: 900,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+
+
+
+
+
+
+// Form Part
+(function($) {
+
+	"use strict";
+
+
+  // Form
+	var contactForm = function() {
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					subject: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 5
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					subject: "Please enter your subject",
+					email: "Please enter a valid email address",
+					message: "Please enter a message"
+				},
+				/* submit via ajax */
+				
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/sendEmail.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeIn();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+
+		               	setTimeout(function(){
+				               $('#form-message-success').fadeOut();   
+		               	}, 8000);
+
+		               	setTimeout(function(){
+				               $submit.css('display', 'none').text(waitText);  
+		               	}, 1400);
+
+		               	setTimeout(function(){
+		               		$( '#contactForm' ).each(function(){
+											    this.reset();
+											});
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		} // end submitHandler
+
+			});
+		}
+	};
+	contactForm();
+
+})(jQuery);
